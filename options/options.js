95d96c3e99a8766dc63ca4329713068c0a83b60f
Crowdbotics <team@crowdbotics.com>
2023-12-09 16:53:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-cherry-44936",
+  url: "https://proud-cherry-44936.botics.co",
+  api: "https://proud-cherry-44936.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
